6fac4ce132414a27994e39ed4644d816fed47945
Nrentzilas <nrentzilas@gmail.com>
2025-05-21 10:06:16 +0300

    Fixed SiteLayout.tsx export/imports

--- app/[lang]/portfolio/page.tsx
+++ app/[lang]/portfolio/page.tsx
@@ -1,4 +1,5 @@
-import { SiteLayout } from "../../components/site-layout"
+//Σωστο import
+import SiteLayout from "../../components/site-layout"
 import PortfolioPage from "../../components/portfolio-page"
 
 export default function Portfolio({ params: { lang } }: { params: { lang: string } }) {
